7513d7651f9f636123bfae4a3d6b949c7e520b0e
xingzepeng <1007817310@qq.com>
2021-04-06 23:05:53 +0800

    列表组件和头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <GlobalHeader :user="userData"></GlobalHeader>
+    <ColumnList :list="list" ></ColumnList>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+const userData:UserProps = {
+  isLogin: false,
+  name: 'Viking'
+}
+const testData:ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marke/5ee22dd58b3c4520012b9470.jpg?x-oss-process=image/resize,m-pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'test2的专栏',
+    description: '这是test2的专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marke/5ee22dd58b3c4520012b9470.jpg?x-oss-process=image/resize,m-pad,h_100,w_100'
+  },
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marke/5ee'
+  },
+  {
+    id: 1,
+    title: 'test1的专栏',
+    description: '这是test1的专栏，有一段非常有意思的简介，可以更新一下欧'
+    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marke/5ee22dd58b3c4520012b9470.jpg?x-oss-process=image/resize,m-pad,h_100,w_100'
+  }
+]
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    ColumnList,
+    GlobalHeader
+  },
+  setup () {
+    return {
+      list: testData,
+      userData
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a href="#" class="navbar-brand">读者专栏</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light">登陆</a></li>
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light">注册</a></li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item"><a href="#" class="btn btn-outline-light">您好{{user.name}}</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface UserProps {
+    isLogin: boolean;
+    name?: string;
+    id?: number;
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      requir: true
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
