0e8dcbbb325c59a866d75822b2118178216b2260
“xzpCSCKFWMS <l>
2021-04-14 23:46:52 +0800

    fix(vue-router): vue3中vue-router的使用

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="container">
     <GlobalHeader :user="userData"></GlobalHeader>
-    <validate-form @form-submit='onFormSubmit'>
-      <div class="mb-3">
-        <label for="exampleInputEmail3" class="form-label"> 邮箱 </label>
-        <validate-input :rules="emailRules" v-model="inputVal" type="text" placeholder="请输入邮箱"></validate-input>
-        {{inputVal}}
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">密码</label>
-        <validate-input :rules="pasRules" v-model="inputPas" type="password" placeholder="请输入密码"></validate-input>
-        {{inputPas}}
-      </div>
-      <template #submit>
-        <span class="btn btn-danger">Submit</span>
-      </template>
-    </validate-form>
+    <router-view></router-view>
+    <footer class="text-center py-4 text-secondary bg-light mt-6">
+      <small>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">
+            ©2021读者专栏
+          </li>
+          <li class="list-inline-item">
+            课程
+          </li>
+          <li class="list-inline-item">
+            文档
+          </li>
+          <li class="list-inline-item">
+            联系
+          </li>
+          <li class="list-inline-item">
+            更多
+          </li>
+        </ul>
+      </small>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
+// import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
+// import ValidateForm from './components/ValidateForm.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 const userData:UserProps = {
   isLogin: true,
@@ -33,30 +40,30 @@ const userData:UserProps = {
 export default defineComponent({
   name: 'App',
   components: {
-    GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    GlobalHeader
+    // ValidateInput,
+    // ValidateForm
   },
   setup () {
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱格式' }
-    ]
-    const pasRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' }
-    ]
-    const inputVal = ref('')
-    const inputPas = ref('')
-    const onFormSubmit = (res: boolean) => {
-      console.log('res', res)
-    }
+    // const emailRules: RulesProp = [
+    //   { type: 'required', message: '电子邮箱地址不能为空' },
+    //   { type: 'email', message: '请输入正确的电子邮箱格式' }
+    // ]
+    // const pasRules: RulesProp = [
+    //   { type: 'required', message: '密码不能为空' }
+    // ]
+    // const inputVal = ref('')
+    // const inputPas = ref('')
+    // const onFormSubmit = (res: boolean) => {
+    //   console.log('res', res)
+    // }
     return {
-      userData,
-      emailRules,
-      inputVal,
-      inputPas,
-      pasRules,
-      onFormSubmit
+      userData
+      // emailRules,
+      // inputVal,
+      // inputPas,
+      // pasRules,
+      // onFormSubmit
     }
   }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,32 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import ColumnDetail from './views/ColumnDetail.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+const routerHistory = createWebHistory()
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/column',
+      name: 'column',
+      component: ColumnDetail
+    }
+  ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
