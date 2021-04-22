adce9c4a5b165eafca7ad832ddb548d4dcb45023
xzp <xingzepeng@geo-compass.com>
2021-04-22 17:32:52 +0800

    fix(helper.ts): 数据转换函数

--- src/helper.ts
+++ src/helper.ts
@@ -41,3 +41,16 @@ export function addColumnAvatar (data: ColumnProps | UserProps, width: number, h
     }
   }
 }
+
+export const arrToObj = <T extends {_id?:string}>(arr:Array<T>) => {
+  return arr.reduce((prev, current) => {
+    if (current._id) {
+      prev[current._id] = current
+    }
+    return prev
+  }, {} as { [key: string]: T })
+}
+
+export const objToArr = <T>(obj: {[key:string]: T}) => {
+  return Object.keys(obj).map(key => obj[key])
+}
